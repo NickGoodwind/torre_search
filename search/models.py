e433a435f213edcf6d61ef91fdbddec5f6e21ca4
NickGoodwind <nicholas.gooding@talent.com>
2023-09-07 12:19:26 -0500

    Add url dependency

--- search/models.py
+++ search/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Search(models.Model):
+    datetime = models.DateTimeField("date of search", auto_now_add=True)
+    query = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.query
+
+
+class Individual(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=32, default=None)
+    title = models.CharField(max_length=64, default=None, null=True)
+    link = models.CharField(max_length=32, default=None, null=True)
+
+    def __str__(self):
+        return self.name
