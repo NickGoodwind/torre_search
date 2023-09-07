a51ba950d31667214797a6ab046ac6eaf39dd4b6
NickGoodwind <nicholas.gooding@talent.com>
2023-09-07 12:47:32 -0500

    Redefine the Individual model and add migrations

--- search/models.py
+++ search/models.py
@@ -16,5 +16,16 @@ class Individual(models.Model):
     title = models.CharField(max_length=64, default=None, null=True)
     link = models.CharField(max_length=32, default=None, null=True)
 
+    def decode(obj):
+        name = obj['name']
+        username = obj['username']
+        title = obj['professionalHeadline']
+        link = f"https://torre.ai/{username}"
+        res, action =  Individual.objects.update_or_create(
+            username=username,
+            defaults={"name": name, "username": username, "title": title,"link": link}
+        )
+        return res
+
     def __str__(self):
         return self.name
