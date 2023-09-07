1a96fe56843e7b809b567512618e979095e2f4bb
NickGoodwind <nicholas.gooding@talent.com>
2023-09-07 12:24:51 -0500

    Update views

--- search/urls.py
+++ search/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("history", views.HistoryView.as_view(), name="history"),
-    path("detail/<int:id>/", views.DetailView.as_view(), name="detail"),
+    path("detail/<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("search", views.search, name="search"),
-    path("favorite", views.favorite, name="favorite"),
-    path("save", views.save, name="save"),
+    path("favorite/<int:pk>", views.favorite, name="favorite"),
+    path("save/<int:pk>", views.save, name="save"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+from django_tables2 import SingleTableView
+from .models import Individual, Search
+import requests, json
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(render(request, "search/index.html", {"title": "My amazing contact finder in Torre.ai"}))
+
+
+def search(request):
+    query = request.GET['q']
+    if not query:
+        return render(
+            request,
+            "search/index.html",
+            {
+                "error_message": "You didn't select a choice.",
+            },
+        )
+
+    # Process search here and add history
+
+    table = SearchTable(resultSet)
+    table.paginate(per_page=10)
+
+    return render(request, "search/results.html", {"table": table, "title": "Search results"})
+
+
+class DetailView(generic.DetailView):
+    model = Individual
+    template_name = "search/detail.html"
+
+
+class HistoryView(SingleTableView):
+    model = Search
+    table_class = HistoryTable
+    template_name = "search/history.html"
+    context_object_name = "latest_query"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Search.objects.order_by("-datetime")
+
+
+def favorite(request, pk):
+    return HttpResponse("You're favoriting an individual %s." % pk)
+
+
+def save(request, pk):
+    return HttpResponse("You're saving an individual %s." % pk)
