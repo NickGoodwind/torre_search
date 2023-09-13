75c8e565afc3645c62edf4b31eefc7807b3c06ef
NickGoodwind <nicholas.gooding@talent.com>
2023-09-13 12:51:18 -0500

    New project structure:
    - root
      - apps
        - app1
        - app2
      - backend
      - frontend

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for torre_search project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("search.urls")),
+    path("", include("apps.search.urls")),
     # path("__debug__/", include("debug_toolbar.urls")),
 ]
